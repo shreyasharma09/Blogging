c155827d4811d5aede9e3214427de71e7170e5b2
shreyasharma09 <shreya.sharma9m@gmail.com>
2024-11-05 01:52:40 +0530

    blogging project

---
+++ src/Pages/Error.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Errorcomp from '../Components/ErrorComponent/Errorcomp'
+
+const Error = () => {
+  return (
+    <div>
+        
+        <Errorcomp/>
+        
+    </div>
+  )
+}
+
+export default Error
